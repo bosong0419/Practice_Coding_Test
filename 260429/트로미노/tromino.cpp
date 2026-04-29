5c9c36328d8f441ed782eeabbbd996962f26dbf5
bosong0419 <57433750+bosong0419@users.noreply.github.com>
2026-04-29 18:59:09 +0900

    Title: 트로미노, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260429/트로미노/tromino.cpp
+++ 260429/트로미노/tromino.cpp
@@ -14,31 +14,25 @@ int main() {
         for(int j = 0; j < m; j++)
             cin >> grid[i][j];
 
-
     int max_sum = 0;
-    int temp;
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < m; j++) {
+            int temp;
             if(!(i + 2 >= n)) {
                 temp = grid[i][j] + grid[i + 1][j] + grid[i + 2][j];
-                max_sum = (temp > max_sum) ? temp : max_sum;
+                max_sum = max(temp, max_sum);
             }
             if(!(j + 2 >= m)) {
                 temp = grid[i][j] + grid[i][j + 1] + grid[i][j + 2];
-                max_sum = (temp > max_sum) ? temp : max_sum;
+                max_sum = max(temp, max_sum);
             }
             if(!(i + 1 >= n || j + 1 >= m)) {
-                temp = grid[i][j] + grid[i + 1][j] + grid[i + 1][j + 1];
-                max_sum = (temp > max_sum) ? temp : max_sum;
-
-                temp = grid[i][j] + grid[i][j + 1] + grid[i + 1][j + 1];
-                max_sum = (temp > max_sum) ? temp : max_sum;
-
-                temp = grid[i][j] + grid[i][j + 1] + grid[i + 1][j];
-                max_sum = (temp > max_sum) ? temp : max_sum;
-
-                temp = grid[i + 1][j] + grid[i][j + 1] + grid[i + 1][j + 1];
-                max_sum = (temp > max_sum) ? temp : max_sum;
+                int sum_2x2 = grid[i][j] + grid[i + 1][j] + grid[i][j + 1] + grid[i + 1][j + 1];
+                max_sum = max({max_sum, 
+                              sum_2x2 - grid[i][j],
+                              sum_2x2 - grid[i + 1][j],
+                              sum_2x2 - grid[i][j + 1],
+                              sum_2x2 - grid[i + 1][j + 1]});
             }
         }
     }
