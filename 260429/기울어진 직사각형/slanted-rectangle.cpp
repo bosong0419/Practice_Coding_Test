b9c2d64e4e2b7ed2bf76d16df1dc90cb998a1737
bosong0419 <57433750+bosong0419@users.noreply.github.com>
2026-04-29 22:38:02 +0900

    Title: 기울어진 직사각형, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260429/기울어진 직사각형/slanted-rectangle.cpp
+++ 260429/기울어진 직사각형/slanted-rectangle.cpp
@@ -3,33 +3,25 @@
 #include <algorithm>
 using namespace std;
 
-int dx[4] = {-1, -1, 1, 1};
-int dy[4] = {1, -1, -1, 1};
 
-int get_rectangle_sum(int x, int y, int width, int height, const vector<vector<int>>& grid) {
+
+int get_rectangle_sum(int x, int y, int width, int height, int n, const vector<vector<int>>& grid) {
     int sum = 0;
-    int curr_x = x;
-    int curr_y = y;
-    for(int i = 0; i < height; i++) {
-        curr_x += dx[0];
-        curr_y += dy[0];
-        sum += grid[curr_x][curr_y];
-    }
-    for(int i = 0; i < width; i++) {
-        curr_x += dx[1];
-        curr_y += dy[1];
-        sum += grid[curr_x][curr_y];
-    }
-    for(int i = 0; i < height; i++) {
-        curr_x += dx[2];
-        curr_y += dy[2];
-        sum += grid[curr_x][curr_y];
-    }
-    for(int i = 0; i < width; i++) {
-        curr_x += dx[3];
-        curr_y += dy[3];
-        sum += grid[curr_x][curr_y];
-    }
+    int dx[4] = {-1, -1, 1, 1};
+    int dy[4] = {1, -1, -1, 1};
+    int move[4] = {height, width, height, width};
+    
+    for(int i = 0; i < 4; i++)
+        for(int j = 0; j < move[i]; j++) {
+            x += dx[i];
+            y += dy[i];
+
+            if(x < 0 || x >= n || y < 0 || y >= n)
+                return 0;
+
+            sum += grid[x][y];
+        }
+
     return sum;
 }
 
@@ -45,24 +37,11 @@ int main() {
             cin >> grid[i][j];
 
     int max_sum = 0;
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < n; j++) {
-            for(int w = 1; w < n; w++) {
-                for(int h = 1; h < n; h++) {
-                    int new_i_h = i + dx[0] * h;
-                    int new_j_h = j + dy[0] * h;
-                    if(new_i_h < 0 || new_i_h >= n || new_j_h < 0 || new_j_h >= n) continue;
-                    int new_i_w = new_i_h + dx[1] * w;
-                    int new_j_w = new_j_h + dy[1] * w;
-                    if(new_i_w < 0 || new_i_w >= n || new_j_w < 0 || new_j_w >= n) continue;
-                    int new_i_h2 = new_i_w + dx[2] * h;
-                    int new_j_h2 = new_j_w + dy[2] * h;
-                    if(new_i_h2 < 0 || new_i_h2 >= n || new_j_h2 < 0 || new_j_h2 >= n) continue;
-                    int current_sum = get_rectangle_sum(i, j, w, h, grid);
-                    max_sum = max(current_sum, max_sum);
-                }
-            }
-        }
-    }
+    for(int i = 0; i < n; i++) 
+        for(int j = 0; j < n; j++) 
+            for(int w = 1; w < n; w++) 
+                for(int h = 1; h < n; h++) 
+                    max_sum = max(get_rectangle_sum(i, j, w, h, n, grid), max_sum);
     cout << max_sum << "\n";
+    return 0;
 }
