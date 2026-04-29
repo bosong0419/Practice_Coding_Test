923f54b3f72ab361e8fb3531f8fec4885f080538
bosong0419 <57433750+bosong0419@users.noreply.github.com>
2026-04-30 04:19:48 +0900

    Title: 최단 Run Length 인코딩, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260430/최단 Run Length 인코딩/shortest-run-length-encoding.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+int run_length_encoding(string str) {
+    string encoding_str;
+    bool new_variable = true;
+    int count = 0;
+    for(int i = 0; i < str.length(); i++) {
+        if(new_variable) {
+            encoding_str += str[i];
+            new_variable = false;
+        }
+        count++;
+        if(str[i] != str[i + 1] || i == str.length() - 1) {
+            encoding_str += to_string(count);
+            count = 0;
+            new_variable = true;
+        }
+    }
+    return encoding_str.length();
+}
+
+int main() {
+    string str;
+    cin >> str;
+    int min_length = 1e9;
+    for(int i = 0; i < str.length(); i++) {
+        min_length = min(min_length, run_length_encoding(str));
+        rotate(str.begin(), str.begin() + 1, str.end());
+    }
+    cout << min_length << "\n";
+}
