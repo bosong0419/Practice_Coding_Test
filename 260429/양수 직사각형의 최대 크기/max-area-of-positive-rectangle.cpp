e534129e7e5f63bfcf29b0734afe2f752b17ac23
bosong0419 <57433750+bosong0419@users.noreply.github.com>
2026-04-29 23:35:30 +0900

    Title: 양수 직사각형의 최대 크기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260429/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.cpp
+++ 260429/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.cpp
@@ -14,19 +14,20 @@ int main() {
         for(int j = 0; j < m; j++)
             cin >> grid[i][j];
 
+    auto is_valid_rect = [&](int r1, int c1, int r2, int c2){ 
+        for(int i = r1; i <= r2; i++)
+            for(int j = c1; j <= c2; j++)
+                if(grid[i][j] <= 0) return false;
+        return true;
+    };
+
     int max_size = -1;
     for(int x1 = 0; x1 < m; x1++)
         for(int x2 = x1; x2 < m; x2++)
             for(int y1 = 0; y1 < n; y1++)
-                for(int y2 = y1; y2 < n; y2++) {
-                    bool is_minus = false;
-                    for(int i = y1; i <= y2; i++)
-                        for(int j = x1; j <= x2; j++) {
-                            if(grid[i][j] <= 0) is_minus = true;
-                        }
-                    if(!is_minus) {
+                for(int y2 = y1; y2 < n; y2++) 
+                    if(is_valid_rect(y1, x1, y2, x2)) 
                         max_size = max(max_size, (y2 - y1 + 1) * (x2 - x1 + 1));
-                    }
-                }
+
     cout << max_size;
 }
