5d774a2a6fd5ec1bca00d3bc433ee691c152c860
bosong0419 <57433750+bosong0419@users.noreply.github.com>
2026-05-01 00:06:41 +0900

    [Passed] 최적의 십자 모양 폭발 (20ms, 0MB)

---
+++ 260501/최적의 십자 모양 폭발/best-cross-shape-bomb.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void copy(vector<vector<int>>& copy_grid, const vector<vector<int>>& grid) {
+    int n = copy_grid.size();
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < n; j++)
+            copy_grid[i][j] = grid[i][j];
+}
+
+void boom(int r, int c, vector<vector<int>>& copy_grid) {
+    int n = copy_grid.size();
+    int len = copy_grid[r][c];
+    for(int i = 0; i < len; i++) {    
+        if(r + i < n)  copy_grid[r + i][c] = 0;
+        if(r - i >= 0) copy_grid[r - i][c] = 0;
+        if(c + i < n)  copy_grid[r][c + i] = 0;
+        if(c - i >= 0) copy_grid[r][c - i] = 0;
+    }
+}
+
+void gravity(vector<vector<int>>& copy_grid) {
+    int n = copy_grid.size();
+    for(int j = 0; j < n; j++) {
+        int write_idx = n - 1;
+        for(int read_idx = n - 1; read_idx >= 0; read_idx--) {
+            if(copy_grid[read_idx][j]) {
+                if(write_idx != read_idx) {
+                    copy_grid[write_idx][j] = copy_grid[read_idx][j];
+                    copy_grid[read_idx][j] = 0;
+                }
+                write_idx--;
+            }
+        }
+    }
+}
+
+int count_pair(vector<vector<int>>& copy_grid) {
+    int n = copy_grid.size();
+    int count = 0;
+    if(n == 1) return 0;
+    if(n == 2) {
+        if(copy_grid[0][0] == copy_grid[0][1] && copy_grid[0][0]) count++;
+        if(copy_grid[1][0] == copy_grid[1][1] && copy_grid[1][0]) count++;
+        if(copy_grid[0][0] == copy_grid[1][0] && copy_grid[0][0]) count++;
+        if(copy_grid[0][1] == copy_grid[1][1] && copy_grid[0][1]) count++;
+        return count;
+    }
+
+    for(int i = 0 ; i < n; i++) {
+        for(int j = 0; j < n - 1; j++) {
+            if(copy_grid[i][j] && copy_grid[i][j] == copy_grid[i][j + 1])
+                count++;
+        }
+    }
+    for(int j = 0 ; j < n; j++) {
+        for(int i = 0; i < n - 1; i++) {
+            if(copy_grid[i][j] && copy_grid[i][j] == copy_grid[i + 1][j])
+                count++;
+        }
+    }
+    return count;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<vector<int>> grid(n, vector<int>(n));
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < n; j++)
+            cin >> grid[i][j];
+
+    int max_pair = 0;
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            vector<vector<int>> copy_grid(n, vector<int>(n));
+            copy(copy_grid, grid);
+            boom(i, j, copy_grid);
+            gravity(copy_grid);
+            max_pair = max(max_pair, count_pair(copy_grid));
+        }
+    }
+    cout << max_pair << "\n";
+}
