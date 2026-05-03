e3bf7b3af6f7e45fa88021f94428ca88c1fc5ae5
bosong0419 <57433750+bosong0419@users.noreply.github.com>
2026-05-03 21:44:45 +0900

    [Passed] 핀볼게임 (8ms, 0MB)

--- 260503/핀볼게임/pinball-game.cpp
+++ 260503/핀볼게임/pinball-game.cpp
@@ -8,16 +8,16 @@ int dy[4] = {0, 0, 1, -1}; // NSEW
 
 int move(vector<vector<int>>& grid, int r, int c, int dir, int time) {
     int n = grid.size();
-    time++;
-    if(grid[r][c] == 1) 
-        dir = (dir + 2) % 4;
-    else if(grid[r][c] == 2) 
-        dir = (dir ^ 1 + 2) % 4;
-
-    int nr = r + dx[dir];
-    int nc = c + dy[dir];
-    if(nr < 0 || nr >= n || nc < 0 || nc >= n) return ++time;
-    return move(grid, nr, nc, dir, time);
+    while(1) {
+        time++;
+        if(grid[r][c] == 1) 
+            dir ^= 2;
+        else if(grid[r][c] == 2) 
+            dir ^= 3;
+        r += dx[dir];
+        c += dy[dir];
+        if(r < 0 || r >= n || c < 0 || c >= n) return time;
+    }
 }
 
 int main() {
@@ -33,7 +33,7 @@ int main() {
 
     int max_time = 0;
     for(int i = 0; i < n; i++) {
-        int time = 0;
+        int time = 1;
         max_time = max({max_time, move(grid, 0, i, 1, time), move(grid, i, 0, 2, time),
                         move(grid, n - 1, i, 0, time), move(grid, i, n - 1, 3, time)});
     }
